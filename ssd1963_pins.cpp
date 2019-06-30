2a0a79ac90432708c7e458916b474a086bd7d616
Dennis Bemmann <dennis@bemmann.de>
2019-07-01 00:39:44 +0100

    Initial commit

---
+++ ssd1963_pins.cpp
@@ -0,0 +1,4 @@
+#include "ugfx-arduino-gdisp-ssd1963.h"
+
+// pins: reset, cs, rs, wr, rd
+extern const ssd1963_pins_t ssd1963_pins = {26, 15, 28, 27, 33};
---
+++ ugfx-ssd1963-config.h
@@ -0,0 +1,7 @@
+#ifndef TEENSY31_UGFX_SSD1963_CONFIG_H
+#define TEENSY31_UGFX_SSD1963_CONFIG_H
+
+#include <ugfx-arduino.h> // main library
+#include <ugfx-arduino-gdisp-ssd1963.h> // display driver library
+
+#endif // TEENSY31_UGFX_SSD1963_CONFIG_H
